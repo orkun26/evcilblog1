b6c2a0c44502cc985be2ccee8fc15035d50efe97
orkun26 <63598720+orkun26@users.noreply.github.com>
2020-07-16 09:50:18 +0300

    first commit

---
+++ dostedin.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<title>bootstrap ilk tema</title>
+	<meta name="description" content="blog">
+	<meta name="keywords" content="HTML, CSS, JavaScript">
+	<meta name="author" content="orkunozturk">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css" >
+	<link rel="stylesheet" href="style.css" >
+</head>
+
+<style type="text/css"> </style>
+<body>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.5.1.slim.min.js" ></script>
+	<script src="js/popper.min.js" ></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	<div style="background-color: lightgrey;">
+		<!-- ____________________________________________________________________________-->
+		<div class="container header">
+			<div class="row">
+
+				<img src="img/evcilim.png" style="margin-top:10px;margin-left: 20px; width: 250px;">
+				<form class="form-inline">
+					<input type="arama" class="form-control col-auto mr-2" name="" placeholder="ARAMA" style="margin-top:10px;margin-top:20px; margin-left:520px; " >
+					<a href="#" class="btn btn-dark" style="margin-top:20px;">ARA</a>
+				</div>
+			</div>
+
+
+
+
+
+
+			<!-- ____________________________________________________________________________-->
+			<div class="container menu" >
+				<div class="row">
+					<div class="col" style=" " >
+						<button type="button" class="btn btn-info btn-lg btn-block" style="margin:10px; ">Dost Edin
+						</button>
+					</div>
+					<div class="col" style=" " >
+						<button type="button" class="btn btn-info btn-lg btn-block" style=" margin-top:10px;">Dostuna Yuva bul </button>
+					</div>
+					<div class="col">
+						<button type="button" class="btn btn-dark btn-lg btn-block" style=" margin-top:10px;">Sıkça Sorulan Sorular </button>
+					</div>
+				</div>
+			</div>
+
+
+
+
+			<!-- ____________________________________________________________________________-->
+		
+
+
+		<div style="height: 770px; overflow-x: hidden; overflow-y: scroll;">
+			<div class="container paylasimlar" align="center">
+				<div class="row" >
+
+
+					<div class="col-sm">
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+
+
+
+
+
+					<div class="col-sm">
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+
+
+
+
+
+
+
+
+					<div class="col-sm">
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+						<div class="card" style="width: 18rem;margin:20px;">
+							<img src="img/evcilim.jpeg" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+
+		</div>
+
+
+
+		<!-- ____________________________________________________________________________-->
+
+		<div style="background-color: lightgrey;">
+			<div class="container foot" >
+				<div class="row">
+					<h6 style="margin:10px;">HER HAKKI SAKLIDIR </h1>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+			<!-- ____________________________________________________________________________-->
+		</body>
+		</html>
---
+++ yedek/yedek1.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<title>bootstrap ilk tema</title>
+	<meta name="description" content="blog">
+	<meta name="keywords" content="HTML, CSS, JavaScript">
+	<meta name="author" content="orkunozturk">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css" >
+	<link rel="stylesheet" href="style.css" >
+</head>
+<body>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.5.1.slim.min.js" ></script>
+	<script src="js/popper.min.js" ></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+
+		<div class="container">
+			<a href="index.php" class="navbar-brand"><img src="img/evcilim.jpeg" width="80" height="60"></a>
+			
+
+			<div class="collapse navbar-collapse" id="menuac">
+
+				<ul class="navbar-nav ml-auto">
+					<li class="nanv-item ">
+						<a href="index.html" class="nav-link" >ANASAYFA</a>
+					</li>
+
+					<li class="nav-item active">
+						<a href="index.html" class="nav-link">YOUTUBE KANALIM</a>
+					</li>
+
+					<li class="nav-item ">
+						<a href="index.html" class="nav-link">UDEMY KURSLARI</a>
+					</li>
+
+					<li class="nav-item ">
+						<a href="index.html" class="nav-link">ÖRNEK SAYFA</a>
+					</li>
+
+
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="arkaplan" style="background-color: #28A745"></div>
+	<div class="container" >
+		<div class="row " >
+			<div class="col" >
+				<button type="button" class="btn btn-primary btn-lg btn-block">Dost Edin
+				</button>
+			</div>
+			<div class="col">
+				<button type="button" class="btn btn-primary btn-lg btn-block">Dostuna Yuva bul  button</button>
+			</div>
+			<div class="col">
+				<button type="button" class="btn btn-primary btn-lg btn-block">Sıkça Sorulan Sorular </button>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col" style="
+			background-color:red; color:red; padding  :10px;">
+				1 of 2
+			</div>
+			<div class="col" style="padding  :10px;">
+				2 of 2
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<footer>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-bottom">
+
+				<div class="container">
+					<a href="index.php" class="navbar-brand"  data-toggle="collapse" data-target="altmenuac"><span>MAİLLİST KAYIT </span></a>
+					<button class="navbar-toggler" type="button"  >
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="altmenuac">
+
+						<form class="form-inline">
+							<input type="email" class="form-control col-auto mr-2" name="" placeholder="Mail Adresi... "  	>
+							<input type="text" class="form-control col-auto " name="" placeholder="Ad ve Soyad... ">
+							<button class="btn btn-success my-1">Kayıt ol!</button>
+						</form>
+
+						<ul class="navbar-nav ml-auto">
+							<li class="nanv-item ">
+								<a href="index.html" class="nav-link">Gizlilik</a>
+							</li>
+							<li class="nav-item ">
+								<a href="index.html" class="nav-link"  >Koşullar ve Şartlar</a>
+							</li>
+
+
+						</ul>
+					</div>
+				</div>
+			</nav></footer>
+
+		</div>
+
+	</div>	
+</div>
+
+
+</body>
+</html>
